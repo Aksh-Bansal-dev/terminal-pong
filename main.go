71323f57455ea16e3877ad9796b6e29b7fe06935
Aksh Bansal <akshmark@gmail.com>
2022-12-22 13:39:30 +0530

    basic game setup
    - ball move
    - ball spin
    - racket move

--- main.go
+++ main.go
@@ -8,16 +8,21 @@ import (
 )
 
 type Playarea struct {
-	n      int
-	m      int
-	mat    [][]byte
-	p1     int
-	p2     int
-	ball   [2]int
-	batLen int
+	n       int
+	m       int
+	mat     [][]byte
+	p1      int
+	p2      int
+	ball    [2]int
+	ballDir [2]int
+	batLen  int
+	p1Dir   int
+	p2Dir   int
 }
 
 const (
+	frameTime = 50
+
 	P1 = iota
 	P2
 	LEFT_MOVE
@@ -47,13 +52,14 @@ func main() {
 				input := string(b)
 				ch <- input
 				if input == prevChar {
-					debounceTimer = time.Now().Add(time.Millisecond * 50)
+					debounceTimer = time.Now().Add(time.Millisecond * frameTime / 2)
 				}
 				prevChar = input
 			}
 		}
 	}()
 
+	iteration := 0
 	for {
 		reading := true
 		for reading {
@@ -61,7 +67,7 @@ func main() {
 			case stdin, _ := <-ch:
 				if stdin == "a" {
 					playarea.move(P2, LEFT_MOVE)
-				} else if stdin == "d" {
+				} else if stdin == "f" {
 					playarea.move(P2, RIGHT_MOVE)
 				} else if stdin == "h" {
 					playarea.move(P1, LEFT_MOVE)
@@ -72,8 +78,16 @@ func main() {
 				reading = false
 			}
 		}
+		end := false
+		if iteration == 0 {
+			end = playarea.moveBall()
+		}
 		playarea.draw()
+		if end {
+			os.Exit(0)
+		}
 		frameSleep()
+		iteration = (iteration + 1) % 2
 	}
 }
 
@@ -83,6 +97,28 @@ func (p *Playarea) draw() {
 	for i := 0; i < len(mat); i++ {
 		fmt.Printf("%s\n", mat[i])
 	}
+}
+
+func (p *Playarea) moveBall() bool {
+	ball := p.ball
+	p.mat[ball[0]][ball[1]] = ' '
+	ball[0] += p.ballDir[0]
+	ball[1] += p.ballDir[1]
+	if ball[0] == 1 && (p.p1 > ball[1] || p.p1+p.batLen < ball[1]) {
+		p.mat[ball[0]][ball[1]] = 'o'
+		return true
+	} else if ball[0] == p.n && (p.p2 > ball[1] || p.p2+p.batLen < ball[1]) {
+		p.mat[ball[0]][ball[1]] = 'o'
+		return true
+	} else if ball[0] == p.n && (p.p2 <= ball[1] && p.p2+p.batLen >= ball[1]) {
+		p.ballDir = [2]int{-1, p.p2Dir}
+	} else if ball[0] == 1 && (p.p1 <= ball[1] && p.p1+p.batLen >= ball[1]) {
+		p.ballDir = [2]int{1, p.p1Dir}
+	} else {
+		p.mat[ball[0]][ball[1]] = 'o'
+		p.ball = ball
+	}
+	return false
 }
 
 func (p *Playarea) move(player, dir int) {
@@ -97,20 +133,24 @@ func (p *Playarea) move(player, dir int) {
 	if dir == LEFT_MOVE {
 		if player == P1 {
 			if p.p1 > 1 {
+				p.p1Dir = -1
 				p.p1--
 			}
 		} else {
 			if p.p2 > 1 {
+				p.p2Dir = -1
 				p.p2--
 			}
 		}
 	} else {
 		if player == P1 {
 			if p.p1 < p.m-p.batLen {
+				p.p1Dir = 1
 				p.p1++
 			}
 		} else {
 			if p.p2 < p.m-p.batLen {
+				p.p2Dir = 1
 				p.p2++
 			}
 		}
@@ -145,11 +185,21 @@ func newPlayarea(n, m int) Playarea {
 		mat[1][i] = '='
 	}
 	mat[n/2][m/2] = 'o'
-	return Playarea{n: n, m: m, mat: mat, p1: m/2 - 4, p2: m/2 - 4, ball: [2]int{n / 2, m / 2}, batLen: 8}
+	return Playarea{
+		n:       n,
+		m:       m,
+		mat:     mat,
+		p1:      m/2 - 4,
+		p2:      m/2 - 4,
+		p1Dir:   0,
+		p2Dir:   0,
+		ball:    [2]int{n / 2, m / 2},
+		ballDir: [2]int{1, 0},
+		batLen:  8}
 }
 
 func frameSleep() {
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(frameTime * time.Millisecond)
 }
 
 func cls() {
